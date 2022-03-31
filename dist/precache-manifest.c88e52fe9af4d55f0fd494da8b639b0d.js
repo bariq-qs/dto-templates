2b6ea748881e74dfa993d29daee1c524961b4e64
Bariq <bariq.xtkj1.14@gmail.com>
2022-03-31 17:08:15 +0700

    [+] add folder dist

---
+++ dist/precache-manifest.c88e52fe9af4d55f0fd494da8b639b0d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b34c5903b3bdf28d79e",
+    "url": "/css/app.be061d10.css"
+  },
+  {
+    "revision": "600b222f29490cf1b361",
+    "url": "/css/chunk-5cdfeae5.acd8dcd5.css"
+  },
+  {
+    "revision": "ca25c7d65a93abe0ff69",
+    "url": "/css/chunk-vendors.758ff07f.css"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/fonts/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/fonts/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/fonts/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/img/color.c7a33805.png"
+  },
+  {
+    "revision": "28eba56ff8eadb7812ee33e19753b325",
+    "url": "/img/icon-dot.28eba56f.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/img/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "708eba80b7dc6f2901b10f0c85d61646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b34c5903b3bdf28d79e",
+    "url": "/js/app.75ffbcfc.js"
+  },
+  {
+    "revision": "600b222f29490cf1b361",
+    "url": "/js/chunk-5cdfeae5.0ec96b8c.js"
+  },
+  {
+    "revision": "40195926c65facee3ebc",
+    "url": "/js/chunk-5f01f0ce.8762398e.js"
+  },
+  {
+    "revision": "ca25c7d65a93abe0ff69",
+    "url": "/js/chunk-vendors.a8392c03.js"
+  },
+  {
+    "revision": "c332d0c073eb8465614c548b57ae5fa6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
